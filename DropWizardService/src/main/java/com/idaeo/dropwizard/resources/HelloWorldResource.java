340f66bd0f54fee9742e2bba605e32e23004f64b
cdock1029 <conordockry@gmail.com>
2013-11-28 14:27:35 -0800

    another jenkins test change, for auto build on push

--- DropWizardService/src/main/java/com/idaeo/dropwizard/resources/HelloWorldResource.java
+++ DropWizardService/src/main/java/com/idaeo/dropwizard/resources/HelloWorldResource.java
@@ -4,11 +4,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-@Path("/hello/{name}")
+@Path("")
 public class HelloWorldResource {
 
     @GET
+    @Path("/hello/{name}")
     public String getHello(@PathParam("name") String name) {
         return "Hello there, " + name + "!";
+    }
+    
+    @GET
+    @Path("/")
+    public String getHelloWorld() {
+        return "Hello world!";
     }
 }
