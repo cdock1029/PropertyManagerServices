bc157c5973bd16f8b1e5b0e80c86fba4c6e5e89e
Conor Dockry <conordockry@gmail.com>
2013-11-28 15:18:03 -0800

    fix path error

--- DropWizardService/src/main/java/com/idaeo/dropwizard/resources/HelloWorldResource.java
+++ DropWizardService/src/main/java/com/idaeo/dropwizard/resources/HelloWorldResource.java
@@ -4,17 +4,17 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-@Path("")
+@Path("/")
 public class HelloWorldResource {
 
     @GET
-    @Path("/hello/{name}")
+    @Path("hello/{name}")
     public String getHello(@PathParam("name") String name) {
         return "Hello there, " + name + "!";
     }
     
     @GET
-    @Path("/")
+    @Path("hello")
     public String getHelloWorld() {
         return "Hello world!";
     }
