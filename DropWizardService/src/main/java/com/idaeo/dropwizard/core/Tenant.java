73ca3b580a85b6e53eca7b546a233123e806d846
Conor Dockry <conordockry@gmail.com>
2013-11-28 15:35:27 -0800

    Tenant class

---
+++ DropWizardService/src/main/java/com/idaeo/dropwizard/core/Tenant.java
@@ -0,0 +1,28 @@
+package com.idaeo.dropwizard.core;
+
+
+public class Tenant {
+    private final long id;
+    private final String name;
+    private final String email;
+
+    public Tenant(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
